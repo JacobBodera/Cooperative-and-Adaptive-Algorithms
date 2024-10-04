b17095aab12212a2f28be107ea3c01d2d8f43787
Jacob Bodera <jacobbodera@macbookair.home>
2024-10-04 15:52:04 -0400

    Finished assignment 1

--- assignments/assignment1/code/problem5.py
+++ assignments/assignment1/code/problem5.py
@@ -1,30 +1,26 @@
 from graph import WeightedGraph
 
 g = WeightedGraph()
-g.add_node('S')
-g.add_node('A')
-g.add_node('B')
-g.add_node('C')
-g.add_node('D')
-g.add_node('E')
-g.add_node('F')
-g.add_node('G')
-g.add_node('H')
+g.add_node('1')
+g.add_node('2')
+g.add_node('3')
+g.add_node('4')
+g.add_node('5')
+g.add_node('6')
+g.add_node('7')
 
 
-g.add_edge('S', 'A', 3)
-g.add_edge('S', 'D', 4)
-g.add_edge('A', 'B', 4)
-g.add_edge('A', 'D', 5)
-g.add_edge('B', 'C', 3)
-g.add_edge('B', 'H', 4)
-g.add_edge('D', 'E', 2)
-g.add_edge('E', 'B', 5)
-g.add_edge('E', 'C', 4)
-g.add_edge('E', 'F', 4)
-g.add_edge('F', 'H', 5)
-g.add_edge('H', 'G', 1)
+
+g.add_edge('1', '2')
+g.add_edge('1', '3')
+g.add_edge('2', '4')
+g.add_edge('2', '5')
+g.add_edge('3', '6')
+g.add_edge('3', '7')
 
 
-print("BFS:", g.bfs('A', 'G'))
-print("DFS:", g.dfs('A', 'G'))
+
+
+
+# print("BFS:", g.bfs('A', 'G'))
+print("DFS:", g.dfs('1', '7'))
